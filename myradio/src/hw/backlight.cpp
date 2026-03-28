6ccd84c3ef2a8425976850914cadb8b030327ab0
gidano <gidano@gmail.com>
2026-03-28 16:03:24 +0100

    Add files via upload

--- myradio/src/hw/backlight.cpp
+++ myradio/src/hw/backlight.cpp
@@ -4,6 +4,7 @@
 uint8_t g_brightness = 128;
 
 void hw_backlight_init_pwm() {
+  if (TFT_BL < 0) return;
   pinMode(TFT_BL, OUTPUT);
 #if defined(ESP_ARDUINO_VERSION_MAJOR) && (ESP_ARDUINO_VERSION_MAJOR >= 3)
   // v3.x: csatorna kezelést a core intézi
@@ -18,6 +19,7 @@ void hw_backlight_init_pwm() {
 
 void hw_backlight_set(uint8_t v) {
   g_brightness = v;
+  if (TFT_BL < 0) return;
 #if defined(ESP_ARDUINO_VERSION_MAJOR) && (ESP_ARDUINO_VERSION_MAJOR >= 3)
   ledcWrite(TFT_BL, g_brightness);
 #else
